f88668b0db69d9c206a803be64a7d1069d6f96c1
Paul Pod <paulpod@neuromantics.net>
2026-06-01 14:12:19 +0100

    interactive items / letter demo code is working

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,17 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+// request missing items from the tenant 
+router.post('/letter-request', function(req, res) {
+
+    var inforequest = req.session.data['info-request'] 
+
+    console.log("requested: " + inforequest)
+
+       
+    res.redirect("/letter-preview")
+   
+})
+
